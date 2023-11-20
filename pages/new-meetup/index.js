ead287110c69c60c1cbceb1873df6af9fc0a4ae6
somin <smhs12@naver.com>
2023-11-20 16:04:49 +0900

    feat: new meetup 저장 기능 구현

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,9 +1,25 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 export default function NewMeetup() {
-  const onAddMeetup = (enteredMeetup) => {
-    console.log(enteredMeetup);
-    //새 모임 추가 함수
+  const router = useRouter();
+  const onAddMeetup = async (enteredMeetup) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetup),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || "새 글 등록에 실패했습니다.");
+    } else {
+      router.push("/");
+    }
   };
+
   return <NewMeetupForm onAddMeetup={onAddMeetup} />;
 }
